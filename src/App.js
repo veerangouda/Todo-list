1dec508ff1839ac5493826b256b16cb792ac3de5
veeran7 <veerangouda@theinfotiger.com>
2022-06-09 22:54:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+
+  // getting data from local storage
+  let getLocalTodoItems = () => {
+    let list = localStorage.getItem("Todo");
+    // console.log("list", list);
+    if (list) {
+      return JSON.parse(localStorage.getItem("Todo"));
+    } else {
+      return [];
+    }
+  };
+
+  //  One state to track input value and another state is to store all input values in an array
+  const [val, setVal] = useState("");
+  const [todo, setTodo] = useState(getLocalTodoItems());
+
+  // setting todo values into local storage
+  useEffect(() => {
+    localStorage.setItem("Todo", JSON.stringify(todo));
+  }, [todo]);
+
+  // on submitting Add button, todo items diplays down
+  const handleSubmit = () => {
+    if (val === "") {
+      return [];
+    } else {
+      setTodo([...todo, val]);
+      setVal("");
+    }
+  };
+
+  // function to remove all todo items
+  const RemoveAll = () => {
+    setTodo([]);
+  };
+
+  // function to remove each selected todo item
+  const deleteitem = (ind) => {
+    let updateditems  = todo.filter((ele, id) => {
+      return ind !== id;
+    });
+    setTodo(updateditems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cleardata">
+        <button onClick={RemoveAll}> Remove All </button>
+      </div>
+      <h2>TailNode To-Do List </h2>
+      <div className="">
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter Text..."
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <span>
+          <button className="button" onClick={handleSubmit}>
+            Return
+          </button>
+        </span>
+      </div>
+      <div className="">
+        <ul>
+          {todo.map((ele, ind) => {
+            return (
+              <div className="output">
+                <li key={ind}> {ele} </li>
+                <span className="deleteitem">
+                  <i className="fa fa-trash" onClick={() => deleteitem(ind)} />
+                </span>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      
     </div>
   );
 }
